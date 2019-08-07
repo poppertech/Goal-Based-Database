f9a873e8eee2e367ab633b3d513f70b98e742f0f
poppertech <bwynne@poppertech.com>
2019-08-07 08:23:22 -0400

    Added Stored Proc for GetNodeByName

---
+++ Goal-Based-Database/Goal-Based-Database/dbo/Stored Procedures/GetNodeByName.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE GetNodeByName
+	@NodeName VARCHAR(50)
+AS
+BEGIN
+
+	SELECT
+		[node].Id AS NodeId,
+		[node].Name AS NodeName,
+		[node].ParentId AS ParentId,
+		[node].InitialInvestment AS InitialInvestment,
+		[node].InitialPrice AS InitialPrice,
+		[node].IsPortfolioComponent AS IsPortfolioComponent
+	FROM [Node] AS [node]
+	WHERE [node].Name = @NodeName
+
+END
--- Goal-Based-Database/Goal-Based-Database/dbo/Stored Procedures/GetNodesByNetworkId.sql
+++ Goal-Based-Database/Goal-Based-Database/dbo/Stored Procedures/GetNodesByNetworkId.sql
@@ -3,24 +3,24 @@
 AS
 BEGIN
 
-SELECT 
-	[node].Id AS NodeId,
-	[node].Name AS NodeName,
-	[node].ParentId AS ParentId,
-	[node].InitialInvestment AS InitialInvestment,
-	[node].InitialPrice AS InitialPrice,
-	[node].IsPortfolioComponent AS IsPortfolioComponent,
-	[distribution].Id AS DistributionId,
-	[distribution].[Index] AS DistributionIndex,
-	[distribution].Minimum AS Minimum,
-	[distribution].Worst AS Worst,
-	[distribution].Likely AS Likely,
-	[distribution].Best AS Best,
-	[distribution].Maximum AS Maximum
-FROM [Node] AS [node]
-JOIN [Distribution] AS [distribution]
-ON [distribution].NodeId = [node].Id
-WHERE [node].NetworkId = @NetworkId
-ORDER BY NodeId ASC, DistributionIndex ASC
+	SELECT 
+		[node].Id AS NodeId,
+		[node].Name AS NodeName,
+		[node].ParentId AS ParentId,
+		[node].InitialInvestment AS InitialInvestment,
+		[node].InitialPrice AS InitialPrice,
+		[node].IsPortfolioComponent AS IsPortfolioComponent,
+		[distribution].Id AS DistributionId,
+		[distribution].[Index] AS DistributionIndex,
+		[distribution].Minimum AS Minimum,
+		[distribution].Worst AS Worst,
+		[distribution].Likely AS Likely,
+		[distribution].Best AS Best,
+		[distribution].Maximum AS Maximum
+	FROM [Node] AS [node]
+	JOIN [Distribution] AS [distribution]
+	ON [distribution].NodeId = [node].Id
+	WHERE [node].NetworkId = @NetworkId
+	ORDER BY NodeId ASC, DistributionIndex ASC
 
 END
